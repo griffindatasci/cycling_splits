287178ce69d11190e0e279b263e69e282ff877cc
Rob Griffin <117731652+griffindatasci@users.noreply.github.com>
2023-02-28 09:00:01 +0100

    added styling and removed output for missing values

--- app.R
+++ app.R
@@ -2,22 +2,30 @@ library(shiny)
 library(data.table)
 
 ui <- fluidPage(
-  numericInput("dst", "Distance", value=29.0, min=0, max=999),
-  numericInput("spd", "Target Speed", value=44, min=0, max=999),
-  numericInput("int", "Split", value=1, min=1, max=99),
-  tableOutput("split_table")
+    includeHTML("www/page_header.html"),
+    includeHTML("www/page_top.html"),
+    includeCSS("www/style.css"),
+    
+    # Get distance, target speed and split interval
+    tags$input(name="dst", placeholder="Distance", type="number", value=NA, min=1),
+    tags$input(name="spd", placeholder="Speed",    type="number", value=NA, min=1),
+    tags$input(name="int", placeholder="Interval",    type="number", value=NA, min=1),
+
+    tableOutput("split_table")
 )
 
 server <- function(input, output) {
   output$split_table <- renderTable({
-    split_table <- data.table(Distance=seq(0,input$dst,input$int))
-    split_table[, Mins:=Distance*(60/input$spd)]
-    if(split_table[, max(Mins)>=60]){
-      split_table[, Time:=sprintf("%02.0f:%02.0f:%04.1f", Mins%/%60, Mins%%60%/%1, Mins%%60%%1*60)]
-    } else {
-      split_table[, Time:=sprintf("%02.0f:%04.1f", Mins%%60%/%1, Mins%%60%%1*60)]
+    if(all(!sapply(input, is.na))) {
+      split_table <- data.table(Distance=unique(c(seq(0,input$dst,input$int), input$dst)))
+      split_table[, Mins:=Distance*(60/input$spd)]
+      if(split_table[, max(Mins)>=60]){
+        split_table[, Time:=sprintf("%02.0f:%02.0f:%04.1f", Mins%/%60, Mins%%60%/%1, Mins%%60%%1*60)]
+      } else {
+        split_table[, Time:=sprintf("%02.0f:%04.1f", Mins%%60%/%1, Mins%%60%%1*60)]
+      }
+      split_table[, .("Distance"=ifelse(.I==.N, "Finish", Distance), Time)]
     }
-    split_table
   })
 }
 
